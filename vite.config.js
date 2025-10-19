a910317fe3bc4b3a3c818aec8b4597dbca324bad
Amr Ahmed <amr448148@gmail.com>
2025-10-19 19:12:20 +0300

    upadate

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Brewly-coffee/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
