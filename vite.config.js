386d57a2172987757950fa86cce5a1d4fd1aca59
Amr Ahmed <amr448148@gmail.com>
2025-10-19 20:30:36 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],base: '/Brewly-coffee',
 
+export default defineConfig({
+  base: '/Brewly-coffee/', 
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
